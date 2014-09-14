f7d5bf8d007baccc8eb0842a8beb27a7d22043f1
maxxwizard <matthewhuynh88@gmail.com>
2014-09-14 19:42:56 -0400

    main file

---
+++ app.js
@@ -0,0 +1,17 @@
+﻿/*
+ * Author: matthewhuynh88@gmail.com
+ * Description: initializes the inhouse bot.
+ */
+
+/*
+var http = require('http');
+var port = process.env.port || 1337;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Bot server is running\n');
+    
+}).listen(port);
+ */
+
+var bot = require("./bot");
+bot.start();
