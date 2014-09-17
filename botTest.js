fa7b50a9be30997aa54a63ed26b457567b3a8b2b
maxxwizard <matthewhuynh88@gmail.com>
2014-09-17 02:25:07 -0400

    starting to flesh out BotTest and use declared/required objects for Intellisense

--- botTest.js
+++ botTest.js
@@ -3,7 +3,19 @@
  * Description: contains logic for testing all of the bot functionality
  */
 
-var trace = require('./trace.js');
+var Db = require('mongodb').Db,
+    MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server,
+    ReplSetServers = require('mongodb').ReplSetServers,
+    ObjectID = require('mongodb').ObjectID,
+    Binary = require('mongodb').Binary,
+    GridStore = require('mongodb').GridStore,
+    Grid = require('mongodb').Grid,
+    Code = require('mongodb').Code,
+    BSON = require('mongodb').pure().BSON,
+    assert = require('assert'),
+    trace = require('./trace.js'),
+    config = require('./config');
 
 module.exports.TestAllFunctionality = function (DbClient) {
     
@@ -26,7 +38,34 @@ module.exports.TestAllFunctionality = function (DbClient) {
     // test: leaderboard
     // test: name
     
+    var db = new Db(config.mongo.dbName, new Server(config.mongo.host, config.mongo.port), { safe: true });
+    db.open(function (err, db) {
+        if (err) {
+            trace.error("BotTest: couldn't connect to database " + this.host + ":" + this.port);
+            return false;
+        } else {
+            db.dropDatabase(function (err, result) {
+                assert.equal(null, err);
+                trace.debug("BotTest: database dropped!");
+                // insert new season
+                var randSeasonId = new ObjectID();
+                var seasonColl = db.collection("seasons");
+                seasonColl.save({ _id: randSeasonId, seasonNum: 1, startDate: Date(2014, 9, 1), endDate: Date(2014, 12, 31), name: 'Season 1' }, function (err, docs) {
+                    assert.equal(null, err);
+                    trace.debug("BotTest: season 1 inserted");
+                });
+            });
+            /*
+            var randPlayerId = new ObjectId();
+            var randPlayerId2 = new ObjectId();
+            db.collection("players").insert({ _id: randPlayerId, username: 'maxxwizard', rating: 1500, steam64Id: '76561197968837492' }, function (err, docs) { });
+             */
+        }
+    }); // end database connect call
+    
+    
     // test new game creation functionality
+    /*
     DbClient.NewGame("76561197968837492", function (err, newGame) {
         if (err) {
             trace.warn("BotTest: game was not created");
@@ -34,6 +73,7 @@ module.exports.TestAllFunctionality = function (DbClient) {
             trace.log("BotTest: new game #" + newGame.gameNum + " created");
         }
     });
+     */
     
     // test registration functionality
     //var success = DbClient.Register("invalidsteamid", "test123");
