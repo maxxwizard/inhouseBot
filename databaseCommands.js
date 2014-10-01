12d0df16d41084b6f549f1962fc1e4839f44cba4
maxxwizard <matthewhuynh88@gmail.com>
2014-10-01 16:29:46 -0400

    updated dependencies

--- databaseCommands.js
+++ databaseCommands.js
@@ -1,5 +1,5 @@
 ﻿// start our server
-// "C:\Program Files\MongoDB 2.6 Standard\bin\mongod.exe" --dbpath C:\sources\InhouseBot\InhouseBot\db
+// "C:\Program Files\MongoDB 2.6 Standard\bin\mongod.exe" --dbpath C:\sources\InhouseBot\db
 
 // interactive client
 // "C:\Program Files\MongoDB 2.6 Standard\bin\mongo.exe" inhouseBot
