e8cd5753bb130603c022231383b77b27b6c2498c
maxxwizard <matthewhuynh88@gmail.com>
2014-10-04 21:04:58 -0400

    changed players array inside a game database object to have a team designation

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
  */
 
 // High priority
+// TODO: change players array inside a game object to format {id: playerObjId, team: 'Radiant'}
 // TODO: implement !reportWin/!reportLoss in dataAccess
 // TODO: implement Client API (message event handler)
 // TODO: implement registration check prior to every database API call
@@ -17,6 +18,8 @@
 // TODO: investigate whether bot can auto-host lobbies
 // TODO: code analysis over entire project to clean up warnings
 // TODO: add JSDoc comments to every method
+// TODO: after registration check, update the player's username if it has changed
+// TODO: display MMR difference between 2 teams
 
 var bot = require('./lib/bot');
 bot.start();
