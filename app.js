54f31bf5513643796f674dab1f5e6bdfd8a50616
maxxwizard <matthewhuynh88@gmail.com>
2014-10-05 15:26:04 -0400

    verified bot can connect to steam, started architecture for client API

--- app.js
+++ app.js
@@ -4,9 +4,10 @@
  */
 
 // High priority
+// TODO: have bot join MS Guild chat
 // TODO: implement Client API (message event handler)
 // TODO: implement registration check prior to every database API call
-// TODO: have bot join MS Guild chat
+
 
 // Normal priority
 // TODO: migrate workload to Azure
@@ -19,6 +20,8 @@
 // TODO: after registration check, update the player's username if it has changed
 // TODO: display MMR difference between 2 teams
 // TODO: add more test code for DbClient.NewSeason
+// TODO: if friend request from a guild member, auto-add the user
+// TODO: !games should display 'Cancelled' games in last 15 minutes
 
 var bot = require('./lib/bot');
 bot.start();
