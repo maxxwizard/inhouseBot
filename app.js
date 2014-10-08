2f5cdd7bad1c341b3504eb3e879977f2fa31982f
maxxwizard <matthewhuynh88@gmail.com>
2014-10-08 02:35:02 -0400

    updated timeline

--- app.js
+++ app.js
@@ -5,9 +5,10 @@
 
 // High priority
 // TODO: implement !unsign in msg handler
-// TODO: implement Client API (message event handler)
+// TODO: Use mongoose for DB modeling
+// TODO: Use Mocha + should + monky to implement tests
 // TODO: implement registration check prior to every database API call
-
+// TODO: change the DbClient class to use a singleton MongoDb object instead of passing db to every method call
 
 // Normal priority
 // TODO: migrate workload to Azure
@@ -19,10 +20,8 @@
 // TODO: add JSDoc comments to every method
 // TODO: after registration check, update the player's username if it has changed
 // TODO: display MMR difference between 2 teams
-// TODO: add more test code for DbClient.NewSeason
 // TODO: if friend request from a guild member, auto-add the user
 // TODO: !games should display 'Cancelled' or 'Completed' games in last 15 minutes
-// TODO: change the DbClient class to use a singleton MongoDb object instead of passing db to every method call
 // TODO: !sign should sign you into the sole game if there is only one
 // TODO: !cancel and !start should start your game if you are the creator
 // TODO: make !newSeason support names with spaces
